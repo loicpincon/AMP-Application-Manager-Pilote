e244f89fb63d09c24b08c3f7b029aa4412c6cc6f
Loic Pincon <loic.pincon@toulouse.miage.fr>
2019-12-05 11:02:49 +0100

    LPI - init projet

---
+++ back/src/main/java/application/manager/pilote/PiloteApplication.java
@@ -0,0 +1,13 @@
+package application.manager.pilote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiloteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiloteApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/application/manager/pilote/PiloteApplicationTests.java
@@ -0,0 +1,13 @@
+package application.manager.pilote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiloteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
