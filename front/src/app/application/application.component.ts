0cef290988849d30d298aa85a1b67aba1c5042ac
Jbrison <jeremie.brison@gmail.com>
2019-12-06 19:51:04 +0100

    BRJ - init menu + header

--- front/src/app/application/application.component.ts
+++ front/src/app/application/application.component.ts
@@ -1,11 +1,21 @@
 import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material';
+import { FormControl } from '@angular/forms';
 @Component({
     selector: 'app-root',
     templateUrl: './application.component.html',
-    styleUrls: ['./application.component.scss']
+    styleUrls: ['./application.component.css']
 
 })
 export class ApplicationComponent {
 
-    constructor() {}
+    constructor(private _router: Router) {}
+
+    mode = new FormControl('push');
+
+    deconnexion(){
+        localStorage.removeItem('token')
+        this._router.navigate(['login'])
+      }
 }
--- front/src/app/application/pilotage/pilotage.component.ts
+++ front/src/app/application/pilotage/pilotage.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pilotage',
   templateUrl: './pilotage.component.html',
-  styleUrls: ['./pilotage.component.scss']
+  styleUrls: ['./pilotage.component.css']
 })
 export class PilotageComponent implements OnInit {
 
-  constructor(private _router: Router){}
+  constructor(){}
 
   ngOnInit(){
 
-  }
-
-  deconnexion(){
-    localStorage.removeItem('token')
-    this._router.navigate(['login'])
   }
 }
