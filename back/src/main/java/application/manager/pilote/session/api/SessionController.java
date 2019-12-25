aba8c696201e2f5ab0b027ecbab12fb7b246552e
LoïcPinçon <LocPinon@DESKTOP-8S79RRI>
2019-12-26 00:59:04 +0100

    LPI - update service connexion

--- back/src/main/java/application/manager/pilote/session/api/SessionController.java
+++ back/src/main/java/application/manager/pilote/session/api/SessionController.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Callable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,10 +38,10 @@ public class SessionController {
 	 * @param token
 	 * @return
 	 */
-	@GetMapping(path = "/{token}")
+	@GetMapping()
 	@ApiManager("consulter")
-	public Callable<ResponseEntity<UserSession>> consulter(@PathVariable String token) {
-		return () -> ResponseEntity.ok(null);
+	public Callable<ResponseEntity<UserSession>> consulter() {
+		return () -> ResponseEntity.ok(sessionService.getSession());
 	}
 
 }
