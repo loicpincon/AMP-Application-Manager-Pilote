1787f9b3323a88fd11f04372143cba4d8250cca5
Jbrison <jeremie.brison@gmail.com>
2020-01-09 16:43:07 +0100

    BRJ - fix tab

--- front/src/app/authentifie/application/action-user/action-user.component.ts
+++ front/src/app/authentifie/application/action-user/action-user.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild, Input} from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
-import { Instance, UserAction } from '../modele/Application';
+import { Component, OnInit, Input} from '@angular/core';
+import { Instance } from '../modele/Application';
 @Component({
   selector: 'application-action-user',
   templateUrl: './action-user.component.html',
@@ -8,19 +7,13 @@ import { Instance, UserAction } from '../modele/Application';
 })
 export class ActionUserComponent implements OnInit {
   displayedColumns: string[] = ['Version', 'Utilisateur','Action','Date','Status'];
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @Input() instance: Instance;
 
   constructor(){}
 
-  dataSource = new MatTableDataSource<UserAction>();
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator; 
   }
 
-  ngOnChanges(){
-    this.dataSource.data = this.instance.userActions;
-  }
 
 }
