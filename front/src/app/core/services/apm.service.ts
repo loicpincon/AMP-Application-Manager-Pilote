0b52ce65df748b891f59cfb47c66f50168df8a90
Pincon Loic <LocPinon@DESKTOP-8S79RRI>
2020-01-14 15:55:45 +0100

    LPI - fix service

--- front/src/app/core/services/apm.service.ts
+++ front/src/app/core/services/apm.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { ApiManagerService } from 'src/app/core/services/api-manager.service';
 import { Observable } from 'rxjs';
 import { Application, Instance, Serveur, Dockerfile, Log } from 'src/app/authentifie/application/modele/Application';
-import { User, DroitApplicatifLevel, Right} from 'src/app/authentifie/administration/modele/model';
+import { User, DroitApplicatifLevel, Right } from 'src/app/authentifie/administration/modele/model';
 
 
 /**
@@ -93,7 +93,7 @@ export class ApmService {
     }
 
     ajouterApplication(application: Application): Observable<Application> {
-        let params = new HttpParams().set('idUser', localStorage.getItem('USER_TOKEN'));
+        let params = new HttpParams().set('idUser', sessionStorage.getItem('USER_TOKEN'));
         const uri = this.apiManagerService.genereUrlWithParam('Application.ajouter', params).url;
         return this.httpClient.post<Application>(uri, application);
     }
@@ -141,9 +141,9 @@ export class ApmService {
         return this.httpClient.post<any>(uri, body);
     }
 
-    recupererLogsInstance(idC: string): Observable<Log[]>{
-        let params = new HttpParams().set('idContainer',idC)
-        const uri = this.apiManagerService.genereUrlWithParam('DockerLog.recuperer',params).url;
+    recupererLogsInstance(idC: string): Observable<Log[]> {
+        let params = new HttpParams().set('idContainer', idC)
+        const uri = this.apiManagerService.genereUrlWithParam('DockerLog.recuperer', params).url;
         return this.httpClient.get<Log[]>(uri);
     }
 
