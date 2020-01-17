1e338865b022a744be78baa4addf240bccfd61d0
Pincon Loic <LocPinon@DESKTOP-8S79RRI>
2020-01-17 01:15:04 +0100

    LPI - problme script

--- back/src/main/resources/script/unix/deploiement_war_gcp.sh
+++ back/src/main/resources/script/unix/deploiement_war_gcp.sh
@@ -1,14 +1,13 @@
-
 #!/bin/bash
 mypath=$(pwd)
 pathToFolderTemporaire=$1
 pathToFolderTemporaireOldNAme=$2
 pathToFolderTemporaireWithNewFile=$3
 cd $pathToFolderTemporaire
-mkdir $pathToFolderTemporaire"/extract"
+MKDIR $pathToFolderTemporaire"/extract"
 cd $pathToFolderTemporaire"/extract"
-jar -xvf $pathToFolderTemporaireOldNAme
+java -xvf $pathToFolderTemporaireOldNAme
 cd $pathToFolderTemporaire
 cp "gcp.properties" "$pathToFolderTemporaire/extract/WEB-INF/classes/gcp.properties"
-jar -cf $pathToFolderTemporaireWithNewFile "$pathToFolderTemporaire/extract"
+java -cf $pathToFolderTemporaireWithNewFile "$pathToFolderTemporaire/extract"
 cd $mypath
