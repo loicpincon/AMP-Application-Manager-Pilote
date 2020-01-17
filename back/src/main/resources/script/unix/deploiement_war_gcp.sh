a879e6306dd24494cc4b622b74dc8b92a86b5802
Pincon Loic <LocPinon@DESKTOP-8S79RRI>
2020-01-17 01:11:03 +0100

    LPI - update linux script

--- back/src/main/resources/script/unix/deploiement_war_gcp.sh
+++ back/src/main/resources/script/unix/deploiement_war_gcp.sh
@@ -1,13 +1,14 @@
+
 #!/bin/bash
 mypath=$(pwd)
 pathToFolderTemporaire=$1
 pathToFolderTemporaireOldNAme=$2
 pathToFolderTemporaireWithNewFile=$3
 cd $pathToFolderTemporaire
-MKDIR $pathToFolderTemporaire"/extract"
+mkdir $pathToFolderTemporaire"/extract"
 cd $pathToFolderTemporaire"/extract"
-"$JAVA_HOME/jar" -xvf $pathToFolderTemporaireOldNAme
+jar -xvf $pathToFolderTemporaireOldNAme
 cd $pathToFolderTemporaire
 cp "gcp.properties" "$pathToFolderTemporaire/extract/WEB-INF/classes/gcp.properties"
-"$JAVA_HOME/jar" -cf $pathToFolderTemporaireWithNewFile "$pathToFolderTemporaire/extract"
+jar -cf $pathToFolderTemporaireWithNewFile "$pathToFolderTemporaire/extract"
 cd $mypath
