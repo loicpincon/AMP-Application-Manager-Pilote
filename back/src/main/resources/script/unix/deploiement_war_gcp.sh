f064024e619d05600b6607f5d3e91232bcfa828a
Pincon Loic <loic.pincon@toulouse.miage.fr>
2020-01-20 00:43:33 +0100

    LPI - push

---
+++ back/src/main/resources/script/unix/deploiement_war_gcp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mypath=$(pwd)
+pathToFolderTemporaire=$1
+pathToFolderTemporaireOldNAme=$2
+pathToFolderTemporaireWithNewFile=$3
+cd $pathToFolderTemporaire
+rm -rf "$pathToFolderTemporaire/extract"
+mkdir "$pathToFolderTemporaire/extract"
+cd $pathToFolderTemporaire"/extract"
+jar -xvf $pathToFolderTemporaireOldNAme
+cd $pathToFolderTemporaire
+cp "gcp.properties" "$pathToFolderTemporaire/extract/WEB-INF/classes/gcp.properties"
+jar -cf $pathToFolderTemporaireWithNewFile "$pathToFolderTemporaire/extract"
+cd $mypath
