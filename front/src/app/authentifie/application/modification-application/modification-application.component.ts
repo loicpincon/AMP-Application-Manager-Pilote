b72964b05ae7f30e02b2addc9a2c797807d5499a
Jbrison <jeremie.brison@gmail.com>
2020-01-21 11:36:55 +0100

    BRJ - fix modif

--- front/src/app/authentifie/application/modification-application/modification-application.component.ts
+++ front/src/app/authentifie/application/modification-application/modification-application.component.ts
@@ -39,23 +39,25 @@ export class ModificationApplicationComponent implements OnInit {
             typeApp: new FormControl(app.type),
             dockerfilesText: new FormControl(app.dockerfile.file),
             basename: new FormControl(app.baseName),
+            bashApplication: new FormGroup({
+              urlBatch: new FormControl({value:'', disabled: true}),
+            }),
+            warApplication: new FormGroup({
+              nomFichierProperties: new FormControl({value:'', disabled: true}),
+            }),
+            nodeJsApplication: new FormGroup({
+              version: new FormControl({value:'', disabled: true}),
+            })
           });
-
-
+          this.selectType(this.application.type)
           if (this.application.type == "WAR") {
             this.initFormWarApplication(this.application as WarApplication);
           } else if (this.application.type == "BASH") {
             this.initFormBashApplication(this.application as BashApplication);
           } else if (this.application.type == "NODEJS") {
             this.initFormNodeJsApplication(this.application as NodeJsApplication);
-
           }
-
-
         })
-
-
-
         this.apmService.recupererServeur().subscribe(serveurs => {
           this.allServer = serveurs;
         })
@@ -68,27 +70,83 @@ export class ModificationApplicationComponent implements OnInit {
   }
 
   onSubmit(value) {
+    if(this.formulaire.valid){
+      let appTmp: any;
+      switch(value.typeApp){
+        case "WAR":{
+          appTmp = new WarApplication();
+          console.log(value)
+          appTmp.nomFichierProperties = value.warApplication.nomFichierProperties
+          break;
+        }
+        case "BASH":{
+          appTmp = new BashApplication();
+          appTmp.urlBatch = value.bashApplication.urlBatch
+          break;
+        }
+        case "NODEJS":{
+          appTmp = new NodeJsApplication();
+          break;
+        }
+      }
+      appTmp.baseName = value.basename
+      appTmp.type = value.typeApp
+      appTmp.name = value.name
+      appTmp.dockerfile = this.application.dockerfile
+      appTmp.dockerfile.file = value.dockerfilesText
+      appTmp.id = this.application.id
+      appTmp.environnements = this.application.environnements
+      appTmp.livrables = this.application.livrables
 
+      this.apmService.modifierApplication(appTmp).subscribe(data =>{
+        this._snackBar.open('Application modifiée avec succès !', '', {
+          duration: 2000,
+          panelClass: 'customSnackBar'
+        });
+      }, error=>{
+        console.log(error)
+      })
+    }else{
+      this._snackBar.open('Le formulaire n\'est pas valide !', '', {
+        duration: 2000,
+        panelClass: 'errorSnackBar'
+      });
+    }
+  }
+
+  selectType(type: string){
+    console.log(type)
+    switch(type){
+      case "WAR":{
+        this.formulaire.controls['bashApplication'].disable()
+        this.formulaire.controls['nodeJsApplication'].disable()
+        this.formulaire.controls['warApplication'].enable()
+        break;
+      }
+      case "BASH":{
+        this.formulaire.controls['bashApplication'].enable()
+        this.formulaire.controls['warApplication'].disable()
+        this.formulaire.controls['nodeJsApplication'].disable()
+        break;
+      }
+      case "NODEJS":{
+        this.formulaire.controls['nodeJsApplication'].enable()
+        this.formulaire.controls['bashApplication'].disable()
+        this.formulaire.controls['warApplication'].disable()
+        break;
+      }
+    }
   }
 
   initFormWarApplication(war: WarApplication) {
-    this.formulaire.addControl('warApplication', new FormGroup({
-      nomFichierProperties: new FormControl(war.nomFichierProperties),
-    }))
-
+    this.formulaire.controls['warApplication'].setValue({nomFichierProperties:war.nomFichierProperties})
   }
 
   initFormBashApplication(war: BashApplication) {
-    this.formulaire.addControl('bashApplication', new FormGroup({
-      urlBatch: new FormControl(war.urlBatch),
-    }))
-
+    this.formulaire.controls['bashApplicationtion'].setValue({urlBatch:war.urlBatch})
   }
 
   initFormNodeJsApplication(war: NodeJsApplication) {
-    this.formulaire.addControl('nodeJsApplication', new FormGroup({
-      // nomFichierProperties: new FormControl(war.nomFichierProperties),
-    }))
 
   }
 
