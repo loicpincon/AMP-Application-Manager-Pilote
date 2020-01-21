0ca5fce883ac68ca3645d7ec8f20fdfb393698e3
Pincon Loic <loic.pincon@toulouse.miage.fr>
2020-01-21 14:34:13 +0100

    Update ApplicationInfoService.java

--- back/src/main/java/application/manager/pilote/commun/service/ApplicationInfoService.java
+++ back/src/main/java/application/manager/pilote/commun/service/ApplicationInfoService.java
@@ -1,7 +1,13 @@
 package application.manager.pilote.commun.service;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.stereotype.Service;
 
 import application.manager.pilote.commun.modele.ApplicationInfoRessource;
@@ -23,7 +29,19 @@ public class ApplicationInfoService {
 	 * @return
 	 */
 	private String getPomVersion() {
-		return this.getClass().getPackage().getImplementationVersion();
+		String version = this.getClass().getPackage().getImplementationVersion();
+		if (version == null) {
+			try {
+				MavenXpp3Reader reader = new MavenXpp3Reader();
+				Model model;
+				model = reader.read(new FileReader("pom.xml"));
+				version = model.getVersion();
+			} catch (IOException | XmlPullParserException e) {
+				LOG.error(e);
+				version = "indisponible";
+			}
+		}
+		return version;
 	}
 
 }
