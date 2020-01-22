4edb2fe3ad2351ea7ddfed1c8ffed2e7bc94fd57
Pincon Loic <loic.pincon@toulouse.miage.fr>
2020-01-22 13:20:28 +0100

    LPI - init module

---
+++ front/src/app/authentifie/application/administration/administration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'application-administration',
+    templateUrl: './administration.component.html',
+    styleUrls: ['./administration.component.css']
+})
+export class AdministrationApplicationComponent {
+
+}
