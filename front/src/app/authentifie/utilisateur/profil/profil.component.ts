a3e99ca5f7e168e95dc40087e6edeb6701fe73db
Pincon Loic <loic.pincon@toulouse.miage.fr>
2020-01-22 14:42:03 +0100

    LPI - add recup profil

--- front/src/app/authentifie/utilisateur/profil/profil.component.ts
+++ front/src/app/authentifie/utilisateur/profil/profil.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../administration/modele/model';
+import { ApmService } from 'src/app/core/services/apm.service';
 
 @Component({
     selector: 'utilisateur-profil-root',
     templateUrl: './profil.component.html',
     styleUrls: ['./profil.component.css']
 })
-export class UtilisateurProfilComponent {
+export class UtilisateurProfilComponent implements OnInit {
+
+    user: User;
+
+    constructor(private apmservice: ApmService) { }
+
+    ngOnInit(): void {
+        this.apmservice.recupererUser(sessionStorage.getItem('USER_TOKEN')).subscribe(us => {
+            this.user = us;
+        })
+    }
 
 }
