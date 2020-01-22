6fa42343906385252c0059fa731ad251208584f4
Pincon Loic <loic.pincon@toulouse.miage.fr>
2020-01-22 16:29:41 +0100

    LPI - update image

--- front/src/app/authentifie/utilisateur/profil/profil.component.ts
+++ front/src/app/authentifie/utilisateur/profil/profil.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../administration/modele/model';
-import { ApmService } from 'src/app/core/services/apm.service';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ApmService } from 'src/app/core/services/apm.service';
 
 @Component({
     selector: 'utilisateur-profil-root',
@@ -13,9 +13,13 @@ export class UtilisateurProfilComponent implements OnInit {
     user: User;
     formulaire: FormGroup;
     consult: boolean = true;
+    urlPhoto: string;
     constructor(private apmservice: ApmService, private formBuilder: FormBuilder) { }
 
     ngOnInit(): void {
+
+        this.urlPhoto = this.apmservice.getImageProfil(sessionStorage.getItem('USER_TOKEN'));
+
         this.apmservice.recupererUser(sessionStorage.getItem('USER_TOKEN')).subscribe(us => {
             this.user = us;
             this.formulaire = this.formBuilder.group({
