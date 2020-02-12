4b0fb364ccf90f64e7837cc89eaea75876b3cb71
Pincon Loic <loic.pincon@toulouse.miage.fr>
2020-02-12 16:04:04 +0100

    LPI - build

--- back/src/main/java/organisation/application/manager/pilote/PiloteApplication.java
+++ back/src/main/java/organisation/application/manager/pilote/PiloteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "organisation.apimanager", "organisation.application" })
+@ComponentScan({ "organisation" })
 public class PiloteApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -17,6 +17,6 @@ public class PiloteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiloteApplication.class, args);
-			}
+	}
 
 }
